b7632462570194c960947c07305bb8d635b10384
Xiuqi Zhang <111311365+zhangxiuqi113@users.noreply.github.com>
2024-02-08 00:29:50 -0500

    Create VariableExercise1.py

---
+++ VariableExercise1.py
@@ -0,0 +1,7 @@
+# ask the user for two numbers and calculate their average
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+average = (num1 + num2) / 2
+
+# print out the average
+print("The average of", num1, "and", num2, "is:", average)
